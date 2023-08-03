137444bdaac1a27a29f1755b34130d9401a86e2f
TonyShikhovtsev <ssvsr2@gmail.com>
2023-08-04 00:09:19 +0500

    Добавлены функции расчета общей стоимости и уровень скидки для конкретного товара. Подготовлены тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+def test_calculate_total_price():
+    item = Item("Тестовый товар", 10.0, 3)
+    expected_total_price = 30.0
+    result = item.calculate_total_price()
+    assert result == expected_total_price
+
+def test_apply_discount():
+    item = Item("Тестовый товар", 100.0, 1)
+    item.apply_discount()
+    expected_price = 50.0
+    result = item.price
+    assert result == expected_price
+
