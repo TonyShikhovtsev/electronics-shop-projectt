cace6fd3c55a5f62fca5b90fed7c4eca55418718
TonyShikhovtsev <ssvsr2@gmail.com>
2023-08-10 18:24:34 +0500

    Внесены следующие изменения: в класс `Item`:
    - атрибут `name` сделан приватным
    - добавлен геттер и сеттер для `name`, используя @property
    - в сеттере `name` проверяно, что длина наименования товара не больше 10 симвовов. В противном случае, обрезать строку (оставить первые 10 символов).
    
    Добавлены в `Item` следующие методы:
    - `instantiate_from_csv()` - класс-метод, инициализирующий экземпляры класса `Item` данными из файла _src/items.csv_
    - `string_to_number()` - статический метод, возвращающий число из числа-строки
    > Для работы с csv-файлом используйте модуль `csv` и метод [`DictReader`]
    
    Добавлены тесты для новых методов в `tests/test_item.py`

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,21 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            name = name[:10]
+        else:
+            self.__name = name
 
     def calculate_total_price(self) -> float:
         """
@@ -34,3 +48,18 @@ class Item:
         self.price *= self.pay_rate
         return self.price
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        Инициализирует экземпляры класса Item данными из CSV файла.
+        """
+        with open('../src/items.csv', newline='', encoding='utf-8-sig') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                print(row['name'], row['price'], row['quantity'])
+                cls(row['name'], row['price'], row['quantity'])
+
+    @staticmethod
+    def string_to_number(string):
+        """Преобразует строку в число."""
+        return float(string)
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,6 +2,13 @@
 import pytest
 from src.item import Item
 
+
+def test_name_getter_setter(self):
+    item = Item("Product", 10.0, 5)
+    self.assertEqual(item.name, "Product")
+
+    item.name = "LongProductName"
+    self.assertEqual(item.name, "LongProdu")
 def test_calculate_total_price():
     item = Item("Тестовый товар", 10.0, 3)
     expected_total_price = 30.0
@@ -14,4 +21,16 @@ def test_apply_discount():
     expected_price = 50.0
     result = item.price
     assert result == expected_price
+
+def test_string_to_number(self):
+    number_str = "15.75"
+    converted_number = Item.string_to_number(number_str)
+    self.assertEqual(converted_number, 15.75)
+
+def test_instantiate_from_csv(self):
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 3
+    assert Item.all[0].name == "Product1"
+    assert Item.all[0].price == "10.0"
+    assert Item.all[0].quantity == "5"
 
